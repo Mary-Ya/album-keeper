6bf3c96c74b0ad43c6a3694dd6ea10b89ffa1b10
Mary Ya <workingmarysmail@gmail.com>
2017-03-12 15:21:56 +0700

    add 404 page

---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class NotFound extends Component {
+  render() {
+    return(
+      <div>
+        <h1>404: No page on this address</h1>
+        <p>Try to use menu on the top of the page</p>
+      </div>
+    )
+  }
+}
+
+export default NotFound;
--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import { IndexRoute, Route } from 'react-router';
 import App from './containers/App';
 import SearchContainer from './containers/Search';
 import SavedContainer from './containers/Saved';
+import NotFound from './components/NotFound';
 
 const routes = (
   <Route path='/' component={App}>
     <IndexRoute component={SearchContainer}/>
     <Route path='saves' component={SavedContainer} />
+    <Route path="*" component={NotFound} />
   </Route>
 );
 
